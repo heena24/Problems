a558eeaaf7d91d221c7b5166cbef80f6a9f9528a
heena1stdibs <heena.hussain@1stdibs.com>
2018-07-18 00:27:06 +0530

    Minore: Used recursion for the bit calculation.

--- src/CountSetBits.java
+++ src/CountSetBits.java
@@ -1,25 +1,23 @@
 public class CountSetBits {
-    public static void main(String[] args) {
 
-        int n = 5;
-        // 1,2,3
+    // O(nLogn) solution
+    public static void main(String[] args)
+    {
+        int n = 3;
         int count = 0;
         for (int i=1; i<=n; i++)
         {
-            count += CountSetBits.getSetBitSummFromInt(i);
+            count += CountSetBits.getSetBitSum(i);
         }
         System.out.println("Count of set bits for n " + n + " is " + count);
     }
 
-    private static int getSetBitSummFromInt(int intNumber)
+    private static int getSetBitSum(int intNumber)
     {
-        int sum = 0;
-        while (intNumber != 0)
-        {
-            int mod = intNumber%2;
-            intNumber = intNumber/2;
-            sum += mod;
+        if (intNumber <= 0) {
+            return 0;
         }
-        return sum;
+
+        return (intNumber%2 == 0 ? 0 : 1) + getSetBitSum(intNumber/2);
     }
 }
